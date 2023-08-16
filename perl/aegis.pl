1838a6d0cf5b4d8f3800e8dcc9994c7b9661eb37
d i r t _ h a c k _  s e v e n <45444393+DRTAC7@users.noreply.github.com>
2023-08-15 23:54:45 -0500

    Update aegis.pl

--- perl/aegis.pl
+++ perl/aegis.pl
@@ -5,8 +5,8 @@
 # Name:           AEGIS - Plaintext Encryption Utility       \ A E G I S /       
 # Copyright:      (c) 2023 telehack.com/u/drtac7              \....|..../        
 # Website:        https://www.github.com/DRTAC7/aegis          \...|.../         
-# Authors:        drtac7, zcj                                   \..|../          
-# Contributors:   ChatGPT                                        \.|./           
+# Authors:        drtac7                                        \..|../          
+# Contributors:   zcj, ChatGPT                                   \.|./           
 # License:        MIT                                             \|/            
 #                                                                                
 # Permission is hereby granted, free of charge, to any person obtaining a copy   
