bb77a1262fff7c5c6a80b9b5a2a3158ad157fcef
Del Cook <drtac7@gmail.com>
2023-08-20 13:47:47 -0500

    v2.0

--- perl/aegis.pl
+++ perl/aegis.pl
@@ -36,11 +36,15 @@ use warnings;
 use MIME::Base64;
 use File::Glob;
 
+# Version Number
+
+my $ver = "2.0";
+
 # compatibility with <5.18 perls
 use English qw( -no_match_vars );
 
 # Create a mapping of Base64 characters to Index65 values
-my @base64_chars = ('A'..'Z', 'a'..'z', 0..9, '+', '/', '=');
+my @base64_chars = ('A'..'Z', 'a'..'z', 0..9, '+', '/', '=',);
 my %base64_to_index65;
 for my $i (0..$#base64_chars) 
 {
@@ -123,6 +127,46 @@ sub create_file
     exit; # Terminate the program after creating the file
 }
 
+sub encrypt_txt_to_agsp 
+{
+    # Take input from the user
+    print "Enter the path to the plaintext file: ";
+    chomp(my $txt_path = <STDIN>);
+
+    # Read the content of the .txt file
+    open(my $txt_file, '<', $txt_path) or die "Cannot open file '$txt_path' for reading: $!";
+    my $content = do { local $/; <$txt_file> };
+    close($txt_file);
+
+    # Convert the input to Index65
+    my $index65_string = '';
+    for my $char (split //, encode_base64($content)) 
+    {
+        if (exists $base64_to_index65{$char}) 
+        {
+            $index65_string .= $base64_to_index65{$char};
+        }
+    }
+
+    # Encrypt the Index65 string
+    my ($encrypted_msg, $key) = encrypt_index65($index65_string);
+
+    # Create the encrypted message with key string
+    my $encrypted_string = "$encrypted_msg" . "l" . "$key";
+
+    # Prompt the user for a filename for the encrypted .agsp file
+    print "Enter the filename (without extension) for the encrypted .agsp file: ";
+    chomp(my $agsp_filename = <STDIN>);
+
+    # Write the encrypted message to a .agsp file
+    open(my $agsp_file, '>', "$agsp_filename.agsp") or die "Cannot open file '$agsp_filename.agsp' for writing: $!";
+    print $agsp_file $encrypted_string;
+    close($agsp_file);
+
+    print "\nFile '$agsp_filename.agsp' created.\n\n";
+    exit; # Terminate the program after creating the file
+}
+
 sub decrypt_file 
 {
     # Take input from the user
@@ -157,6 +201,48 @@ sub decrypt_file
     exit; # Terminate the program after decryption
 }
 
+sub decrypt_agsp_to_txt 
+{
+    # Take input from the user
+    print "Enter the filename (without extension) of the .agsp file to decrypt: ";
+    chomp (my $filename = <STDIN>);
+
+    # Read the content of the .agsp file
+    open(my $file, '<', "$filename.agsp") or die "Cannot open file '$filename.agsp' for reading: $!";
+    my $content = <$file>;
+    close($file);
+
+    # Separate the encrypted message and key using 'l'
+    my ($encrypted_msg, $key) = split 'l', $content;
+
+    # Decrypt the encrypted message
+    my $decrypted_index65 = decrypt_index65_with_key($encrypted_msg, $key);
+
+    my $base64_output = '';
+    for (my $i = 0; $i < length($decrypted_index65); $i += 2) 
+    {
+        my $index = substr($decrypted_index65, $i, 2);
+        if (exists $index65_to_base64{$index}) 
+        {
+            $base64_output .= $index65_to_base64{$index};
+        }
+    }
+
+    my $decoded_output = decode_base64($base64_output);
+
+    # Prompt the user for a filename for the decrypted .txt file
+    print "Enter the filename (without extension) for the decrypted .txt file: ";
+    chomp(my $txt_filename = <STDIN>);
+
+    # Write the decrypted message to a .txt file
+    open(my $txt_file, '>', "$txt_filename.txt") or die "Cannot open file '$txt_filename.txt' for writing: $!";
+    print $txt_file $decoded_output;
+    close($txt_file);
+
+    print "\nFile '$txt_filename.txt' created.\n\n";
+    exit; # Terminate the program after creating the file
+}
+
 sub delete_agsp_files 
 {
     # Get a list of .agsp files in the current directory
@@ -176,54 +262,26 @@ sub delete_agsp_files
     exit; # Terminate the program after deleting .agsp files
 }
 
-print "Choose an option:\n";
-print "1. Encrypt a message\n";
-print "2. Decrypt a message\n";
-print "3. Create a file with encrypted message and key\n";
-print "4. Decrypt a file\n";
+print "\nAEGIS Version $ver for Perl\n\n";
+print "Choose an option:\n\n";
+print "1. Create a file with encrypted message and key\n";
+print "2. Decrypt a file and print message to screen\n";
+print "3. Encrypt an existing .txt file into .agsp\n";
+print "4. Decrypt an existing .agsp file and output message to .txt file\n";
 print "5. Delete all .agsp files in the directory\n";
-print "0. Exit\n";
+print "0. Exit\n\n";
 print "Option: ";
 
 chomp (my $option = <STDIN>);
 
-if ($option == 1) # Encrypt a message
-{
-    print "Enter a message to encrypt: ";
-    chomp (my $input = <STDIN>);
-    my $index65_string = '';
-    for my $char (split //, encode_base64($input)) 
-    {
-        if (exists $base64_to_index65{$char}) 
-        {
-            $index65_string .= $base64_to_index65{$char};
-        }
-    }
-    my ($encrypted_msg, $key) = encrypt_index65($index65_string);
-    print "\nEncrypted Message: $encrypted_msg\n";
-    print "\nKey: $key\n\n";
-} elsif ($option == 2) # Decrypt a message
-{
-    print "Enter the encrypted message: ";
-    chomp (my $encrypted_msg = <STDIN>);
-    print "Enter the key: ";
-    chomp (my $key = <STDIN>);
-    my $decrypted_index65 = decrypt_index65_with_key($encrypted_msg, $key);
-    my $base64_output = '';
-    for (my $i = 0; $i < length($decrypted_index65); $i += 2) 
-    {
-        my $index = substr($decrypted_index65, $i, 2);
-        if (exists $index65_to_base64{$index}) 
-        {
-            $base64_output .= $index65_to_base64{$index};
-        }
-    }
-    my $decoded_output = decode_base64($base64_output);
-    print "\nDecrypted Message: $decoded_output\n\n";
-} elsif ($option == 3) { # Create an encrypted file
+if ($option == 1){ # Create an encrypted file
     create_file();
-} elsif ($option == 4) { # Decrypt an encrypted file
+} elsif ($option == 2) { # Decrypt an encrypted file and print to screen
     decrypt_file();
+} elsif ($option == 3) { # Encrypt an existing file .txt file
+    encrypt_txt_to_agsp();
+} elsif ($option == 4) { # Decrypt an encrypted .agsp file to .txt file
+    decrypt_agsp_to_txt();
 } elsif ($option == 5) { # Delete all .agsp files
     delete_agsp_files();
 } elsif ($option == 0) { # Exit cleanly
